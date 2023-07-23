b6ef13287501a9a3d22bc3a5c9ae39b7b96c8a0f
todaykim <pztdrd@naver.com>
2023-07-23 18:18:16 +0900

    메인페이지 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import DustPage from './pages/DustPage';
 import Root from './pages/Root';
-
+import Main from './Main';
 const App: React.FC = () => {
   const queryClient = new QueryClient();
   const router = createBrowserRouter([
@@ -10,7 +10,10 @@ const App: React.FC = () => {
       path: '/fineDustCheck',
       element: <Root />,
       errorElement: <p>Not Found 😢</p>,
-      children: [{ path: 'fineDust', element: <DustPage /> }],
+      children: [
+        { path: 'fineDust', element: <DustPage /> },
+        { path: '', element: <Main /> },
+      ],
     },
   ]);
 
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../header/NavBar';
-import Main from '../Main';
 
 export default function Root() {
   return (
     <div>
       <NavBar />
       <Outlet />
-      <Main />
     </div>
   );
 }
